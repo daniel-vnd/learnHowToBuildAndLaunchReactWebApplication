0e2004b7469b9f7a44e14d563408ced349abca2e
Daniel Velnic <velnicd@yahoo.com>
2025-04-07 00:20:17 +0300

    Section 5 - Saving and Loading the Count into/from localStorage

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -5,9 +5,27 @@ class Counter extends React.Component {
     this.handleMinusOne = this.handleMinusOne.bind(this);
     this.handleReset = this.handleReset.bind(this);
     this.state = {
-      count: props.count
+      count: 0
     };
   }
+
+  componentDidMount() {
+    const stringCount = localStorage.getItem('count');
+    const count = parseInt(stringCount, 10);
+
+    if(! isNaN(count)) {
+      this.setState(() => ({ count }));
+    }
+  }
+
+  // Save the count when it changes
+  componentDidUpdate(prevProps, prevState) {
+    if(prevState.count !== this.state.count) {
+      localStorage.setItem('count', this.state.count);
+      console.log('componentDidUpdate');
+    }
+  }
+
   handleAddOne() {
     this.setState((prevState) => {
       return {
@@ -41,8 +59,5 @@ class Counter extends React.Component {
   }
 }
 
-Counter.defaultProps = {
-  count: 0
-}
 
 ReactDOM.render(<Counter />, document.getElementById('app'));
